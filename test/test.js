1c52c38f30c72ba78fadd7f5353fcc1ec6c58847
Tmura <Tmura>
2017-01-17 19:04:14 +0200

    red

--- test/test.js
+++ test/test.js
@@ -33,10 +33,10 @@ it('getChange(210,300) should equal [50,20,20]', function(){
 C.getChange = function (totalPayable, cashPaid) {
     'use strict';
     return [50, 20, 20];    // just enough to pass :-)
-};/*
+};
 it('getChange(486,1000) should equal [500, 10, 2, 2]', function(){
     assert.deepEqual(C.getChange(486,1000), [500, 10, 2, 2]);
-})
+})/*
 var C = {};     // C Object simplifies exporting the module
 C.coins = [5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1]
 C.getChange = function (totalPayable, cashPaid) {
